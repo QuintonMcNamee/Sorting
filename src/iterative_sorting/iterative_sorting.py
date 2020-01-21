f04805c7f4dc45af6fff0d8d6c3f062237e9a387
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2020-01-21 11:02:37 -0500

    bubble sort working! woohoo!

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -20,8 +20,22 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
-    return arr
+    looped_counter = 0
+    # 1. Loop through your array
+    for x in arr:
+        for index in range(0, len(arr)-1):
+    #     - Compare each element to its neighbor
+            if arr[index] > arr[index+1]:
+    #     - If elements in wrong position (relative to each other, swap them)
+                looped_counter += 1
+                temp = arr[index]
+                arr[index] = arr[index+1]
+                arr[index+1] = temp
+    # 2. If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1.
+        if looped_counter == 0:
+            return arr
+        else:
+            looped_counter = 0
 
 
 # STRETCH: implement the Count Sort function below
